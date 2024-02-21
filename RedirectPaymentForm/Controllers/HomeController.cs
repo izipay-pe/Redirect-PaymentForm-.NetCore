32cdbb8f009d7c3f8975305ce5ec789380167f21
Jeanpier Araujo <soporteecommerce@izipay.pe>
2024-02-21 11:51:51 -0500

    Ejemplo Redireccion en .NETCore

---
+++ RedirectPaymentForm/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RedirectPaymentForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RedirectPaymentForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PaymentModel pago = new PaymentModel();
+            pago.vads_action_mode = "INTERACTIVE";
+            pago.vads_ctx_mode = "TEST";               /* TEST / PRODUCTION*/
+            pago.vads_currency = "604";                /*Soles = 604 / Dólar = 840*/
+            pago.vads_page_action = "PAYMENT";
+            pago.vads_payment_config = "SINGLE";
+            pago.vads_site_id = "~~CHANGE_ME_ID~~";    
+            pago.vads_version = "V2";
+            pago.KEY = "~~CHANGE_ME_KEY~~";    
+            return View(pago);
+        }  
+    }   
+}
+
---
+++ RedirectPaymentForm/Models/PaymentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedirectPaymentForm.Models
+{
+    public class PaymentModel
+    {
+        public String vads_action_mode { get; set; }
+        public String vads_amount { get; set; }
+        public String vads_ctx_mode { get; set; }
+        public String vads_currency { get; set; }
+        public String vads_page_action { get; set; }
+        public String vads_payment_config { get; set; }
+        public String vads_site_id { get; set; }
+        public String vads_trans_date { get; set; }
+        public String vads_trans_id { get; set; }
+        public String vads_version { get; set; }
+        public String signature { get; set; }
+        public String KEY { get; set; }
+
+    }
+}
